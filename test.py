4d6abc81e788ef3eb0633fbb88905eb5cba71ef3
kenmaro3 <miha.ken.19@gmail.com>
2025-04-01 15:33:16 +0900

    initial commit

---
+++ test.py
@@ -0,0 +1,17 @@
+from apns2.client import APNsClient
+from apns2.payload import Payload
+from apns2.credentials import TokenCredentials
+
+auth_key_path = 'xxx'
+auth_key_id = 'xxx'
+team_id = 'xxx'
+topic = 'xxx'
+
+token_credentials = TokenCredentials(auth_key_path=auth_key_path, auth_key_id=auth_key_id, team_id=team_id)
+client = APNsClient(credentials=token_credentials, use_sandbox=True)
+
+payload = Payload(alert="Hello World!", sound="default", badge=1)
+test_device_token = 'xxx'
+
+print("Sent notification to device:", test_device_token)
+
