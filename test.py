b51d2df249273fed408c2b9fba216db7489b7698
kenmaro3 <miha.ken.19@gmail.com>
2025-04-06 12:40:56 +0900

    [UPD] silent push

--- test.py
+++ test.py
@@ -10,8 +10,11 @@ topic = 'xxx'
 token_credentials = TokenCredentials(auth_key_path=auth_key_path, auth_key_id=auth_key_id, team_id=team_id)
 client = APNsClient(credentials=token_credentials, use_sandbox=True)
 
+# Include a badge value along with alert and sound if desired.
 payload = Payload(alert="Hello World!", sound="default", badge=1)
 test_device_token = 'xxx'
 
 print("Sent notification to device:", test_device_token)
 
+client.send_notification(test_device_token, payload, topic)
+print("Sent notification with badge")
